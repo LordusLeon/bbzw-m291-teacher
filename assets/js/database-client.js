2f645ea04004471a1ee41f89630522740c65dcaa
LordusLeon <105914826+LordusLeon@users.noreply.github.com>
2022-06-18 23:28:43 +0200

    Update database-client.js

--- assets/js/database-client.js
+++ assets/js/database-client.js
@@ -1,22 +1,13 @@
-/*
-Sie müssen nicht wissen, wie executeSqlQuery funktioniert. 
-Sie müssen nur wissen, wie Sie diesen Code verwenden können bei form.js. 
-*/
-
-
 const databaseClient = {
   // Account Daten
   data: {
     url: "https://database-teacher-onlineshop.herokuapp.com/import",
-    // ! Ändern Sie group (al1, al2, al3, b1, ...)
     group: "al2",
-    // ! Ändern Sie pw
     pw: "21e44da8",
     sql: ""
   },
 
   // HTTP Request an die Datenbank
-  // Mitgegeben wird ein SQL Query als String 
   executeSqlQuery: async (sql) => {
     databaseClient.data.sql = sql
     const response = await fetch(databaseClient.data.url, {
@@ -33,8 +24,6 @@ const databaseClient = {
     return result
   },
 
-  // Mitgegeben wird der Name der Tabelle als String, die Felder als Array und die Werte als Array.
-  // Beispiel: databaseClient.insertInto("customers", ["firstname", "email"], [firstname.value, email.value])
   insertInto: async (tableName = "users", fields = ["email"], values = []) => {
     const sql = `INSERT INTO ${tableName} (${fields.join(",")}) VALUES ('${values.join("','")}')`
     return await databaseClient.executeSqlQuery(sql)
